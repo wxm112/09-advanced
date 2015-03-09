a00631b64145e2405404da560613e6b5ba656855
王雪梅 <wxm112@me.com>
2015-03-09 12:30:16 +1100

    finished tdd_bank example

--- tdd_bank/bank.rb
+++ tdd_bank/bank.rb
@@ -1,8 +1,26 @@
 class Bank
   attr_reader :name
+  attr_accessor :accounts
 
   def initialize(name)
     @name = name
+    @accounts = {}
+  end
+
+  def create_account(account_name, deposit)
+    @accounts[account_name] = deposit if deposit > 0
+  end
+
+  def deposit(account_name, amount)
+    @accounts[account_name] += amount if amount > 0
+  end
+
+  def balance(account_name)
+    @accounts[account_name]
+  end
+
+  def withdraw(account_name, amount)
+    @accounts[account_name] -= amount if amount <= @accounts[account_name]
   end
 
 end
--- tdd_bank/spec/bank_spec.rb
+++ tdd_bank/spec/bank_spec.rb
@@ -2,24 +2,70 @@ require_relative 'spec_helper.rb'
 require_relative '../bank.rb'
 
 describe Bank do  
+
+  let(:bank) {Bank.new('TDD Bank')}
+
   describe '.new' do 
     it 'creates a new Bank object' do 
-      bank = Bank.new('TDD Bank')
       expect(bank).to_not eq nil
     end
 
     it 'should have a name' do 
-      bank = Bank.new('TDD Bank')
       expect(bank.name).to eq 'TDD Bank'
+    end
+
+    it 'has no accounts' do 
+      expect(bank.accounts.count).to eq 0
     end
   end
 
-  describe '#create_account' do 
-    it 'creates an account' do 
-      bank = Bank.new('TDD Bank')
+  context 'Bob has $200 in his accounts' do 
+
+    before do 
       bank.create_account('Bob', 200)
-      expect(bank.accounts['Bob']).to eq 200
     end
+
+    describe '#create_account' do 
+      it 'creates an account' do 
+        expect(bank.accounts['Bob']).to eq 200
+      end
+
+      it 'ignores deposit of negative amounts' do 
+        bank.create_account('Jorge', -200)
+        expect(bank.accounts['Jorge']).to eq nil
+      end
+    end
+
+    describe '#deposit' do 
+      it 'deposit an amount from a client into their account' do 
+        bank.deposit('Bob', 300)
+        expect(bank.accounts['Bob']).to eq(200 + 300)
+      end
+
+      it 'ignores deposit of negative amounts' do 
+        bank.deposit('Bob', -1_000_000)
+        expect(bank.accounts['Bob']).to eq 200
+      end
+    end
+
+    describe '#balance' do 
+      it 'returns the balance for client' do 
+        expect(bank.balance('Bob')).to eq 200
+      end
+    end
+
+    describe '#withdraw' do 
+      it 'subtracts money from the account' do 
+        bank.withdraw('Bob', 50)
+        expect(bank.balance('Bob')).to eq(200-50)
+      end
+
+      it 'ignores requests for withdrawals that exceed the balance' do
+        bank.withdraw('Bob', 1_000_000)
+        expect(bank.balance('Bob')).to eq 200
+      end
+    end
+
   end
 
 end
