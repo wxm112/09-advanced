09e78e41e203dfdb5054585db003a66b2cdbb4df
王雪梅 <wxm112@me.com>
2015-03-09 11:00:19 +1100

    added tdd_bank

---
+++ tdd_bank/spec/bank_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'spec_helper.rb'
+require_relative '../bank.rb'
+
+describe Bank do  
+  describe '.new' do 
+    it 'creates a new Bank object' do 
+      bank = Bank.new('TDD Bank')
+      expect(bank).to_not eq nil
+    end
+
+    it 'should have a name' do 
+      bank = Bank.new('TDD Bank')
+      expect(bank.name).to eq 'TDD Bank'
+    end
+  end
+
+  describe '#create_account' do 
+    it 'creates an account' do 
+      bank = Bank.new('TDD Bank')
+      bank.create_account('Bob', 200)
+      expect(bank.accounts['Bob']).to eq 200
+    end
+  end
+
+end
+
---
+++ tdd_bank/spec/spec_helper.rb
@@ -0,0 +1,6 @@
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
+
