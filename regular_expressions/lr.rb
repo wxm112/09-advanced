621c1efa038b8fcdecd2320949301627d446bb56
王雪梅 <wxm112@me.com>
2015-03-16 20:07:11 +1100

    added regular_expreesions

---
+++ regular_expressions/lr.rb
@@ -0,0 +1,10 @@
+# ruby lr.rb simpsons.txt
+
+ARGF.each do |line|
+  # puts line if line =~ /fred/
+  # puts line if line =~ /[Ff]red/
+  # puts line if line =~ /\./
+  # puts line if line =~ /[A-Z][a-z]/
+  # puts line if line =~ /([A-Za-z])\1/
+  # puts line if line =~ /wilma/ || line =~ /fred/
+end
